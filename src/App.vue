e88c81339662d91277bc4d937e195b114781e60c
Aleksey <d_team-lex@bk.ru>
2022-10-04 17:32:15 +0300

    save

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <MySvg />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
+import MySvg from './components/UI/MySvg.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    MySvg,
   },
 };
 </script>
---
+++ src/components/UI/MySvg.vue
@@ -0,0 +1,18 @@
+<template>
+  <img src="" alt="" srcset="" />
+</template>
+
+<script>
+export default {
+  name: 'my-svg',
+  props: {
+    svgName: {
+      type: String,
+      default: '',
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
